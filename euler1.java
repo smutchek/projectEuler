960ff24cef02497b4cb9502981552f20a2ec4d10
smutchek <sarah@mutchek.com>
2019-08-07 22:27:36 -0400

    added public euler1 and the object a

--- euler1.java
+++ euler1.java
@@ -2,15 +2,20 @@ import java.util.ArrrayList;
 public class euler1{
     int sumOfAll;
     ArrayList<Integer> allNums = new ArrayList<Integer>();
+    public euler1(int sum, ArrayList<Integer> all){
+        int sumOfAll = sum;
+        ArrayList<Integer> allNums = all;
+    }
     public static void main(String[] args) {
+        euler1 a = new euler1(0, all);
         for(int i = 1; i<= 1000; i++){
             if(i%3 == 0 || i%5 == 0){
-                allNums.add(i);
+                a.allNums.add(i);
             }
         }
-        for(int j = 0; j <= allNums.length(); j++){
-            sumOfAll += allNums.get(j);
+        for(int j = 0; j <= a.allNums.length(); j++){
+            a.sumOfAll += a.allNums.get(j);
         }
-        System.out.println(sumOfAll);
+        System.out.println(a.sumOfAll);
     }
 }

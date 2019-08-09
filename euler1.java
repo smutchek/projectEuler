2808ba5862fb6cf80aa04d4119cdcaf55f5cb66e
smutchek <sarah@mutchek.com>
2019-08-08 22:02:42 -0400

    answer=234168,got rid of inst field and obj create

--- euler1.java
+++ euler1.java
@@ -1,21 +1,25 @@
-import java.util.ArrrayList;
-public class euler1{
-    int sumOfAll;
-    ArrayList<Integer> allNums = new ArrayList<Integer>();
-    public euler1(int sum, ArrayList<Integer> all){
-        int sumOfAll = sum;
-        ArrayList<Integer> allNums = all;
-    }
+import java.util.ArrayList;
+
+public class euler1 {
+    // wrong - don't need a constructor if you're using a static method. don't need to create  instance fields, just make them variables
+    // public euler1(int sum, ArrayList<Integer> all){
+    // sumOfAll = sum;
+    // allNums = all;
+    // }
     public static void main(String[] args) {
-        euler1 a = new euler1(0, all);
-        for(int i = 1; i<= 1000; i++){
-            if(i%3 == 0 || i%5 == 0){
-                a.allNums.add(i);
+        int sumOfAll = 0;
+        ArrayList<Integer> allNums = new ArrayList<Integer>();
+        // wrong - Don't need to create new object - its a static method
+        // ArrayList<Integer> next = new ArrayList<Integer>();
+        // euler1 a = new euler1(0, next);
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                allNums.add(i);
             }
         }
-        for(int j = 0; j <= a.allNums.length(); j++){
-            a.sumOfAll += a.allNums.get(j);
+        for (int j = 0; j < allNums.size(); j++) {
+            sumOfAll += allNums.get(j);
         }
-        System.out.println(a.sumOfAll);
+        System.out.println(sumOfAll);
     }
 }
